3a4ebbb81578849b945a90b296e63b1e41962460
Christopher Bloom <bloomcb@gmail.com>
2018-10-09 09:09:37 -0700

    Move frags to their own file.

--- source/_patterns/02-molecules/vue-widget/src/vue-fragrance/vue-fragrance.vue
+++ source/_patterns/02-molecules/vue-widget/src/vue-fragrance/vue-fragrance.vue
@@ -40,104 +40,13 @@
 </template>
 
 <script>
+import fragrances from './fragrances';
+
 export default {
   name: 'VueFragranceCombining',
   data() {
     return {
-      fragrances: [
-        {
-          id: 10101,
-          names: {
-            short: 'Lemon Basil & Mandarin',
-            long: 'Lemon Basil & Mandarin Cologne',
-          },
-          cat: 'citrus',
-          pizazz: 0,
-          img: {
-            combiner: 'limebasilmandarin.jpg',
-            spectrum: 'spectrum-lime_basil.jpg',
-          },
-          price: {
-            30: 68.0,
-            100: 136.0,
-          },
-          companion: {
-            warmer: 9900,
-            fresher: 10066,
-          },
-          isStarter: true,
-          isHighlighted: false,
-        },
-        {
-          id: 9900,
-          names: {
-            short: 'Pomegranate Noir',
-            long: 'Pomegranate Noir Cologne',
-          },
-          cat: 'fruity',
-          pizazz: 0,
-          img: {
-            combiner: 'pomegranatenoir.jpg',
-            spectrum: 'spectrum-pomegranate_noir.jpg',
-          },
-          price: {
-            30: 68.0,
-            100: 136.0,
-          },
-          companion: {
-            warmer: undefined,
-            fresher: 12553,
-          },
-          isStarter: true,
-          isHighlighted: false,
-        },
-        {
-          id: 10041,
-          names: {
-            short: 'Grapefruit',
-            long: 'Grapefruit Cologne',
-          },
-          cat: 'citrus',
-          pizazz: 1,
-          img: {
-            combiner: 'grapefruit.jpg',
-            spectrum: 'spectrum-grapefruit.jpg',
-          },
-          price: {
-            30: 68.0,
-            100: 136.0,
-          },
-          companion: {
-            warmer: 27028,
-            fresher: 10101,
-          },
-          isStarter: true,
-          isHighlighted: false,
-        },
-        {
-          id: 10066,
-          names: {
-            short: 'Red Roses',
-            long: 'Red Roses Cologne',
-          },
-          cat: 'light floral',
-          pizazz: 1,
-          img: {
-            combiner: 'redroses.jpg',
-            spectrum: 'spectrum-red_roses.jpg',
-          },
-          price: {
-            30: 68.0,
-            100: 136.0,
-          },
-          companion: {
-            warmer: undefined,
-            fresher: 15968,
-          },
-          isStarter: true,
-          isHighlighted: false,
-        },
-      ],
+      fragrances,
       categories: [
         'citrus',
         'fruity',
